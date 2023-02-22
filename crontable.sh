f8deafdf7554c74d00d36d6fe3bdd4c00414526a
Martin Jimenez Garcia <42785372+marzers42@users.noreply.github.com>
2023-02-22 16:29:34 -0600

    Create crontable.sh

---
+++ crontable.sh
@@ -0,0 +1,10 @@
+# Antes que nada especificaremos donde esta el crontab maestro para ser modificado.
+
+/etc/crontab
+
+#Para acceder al crontab mas reciente accedemos con el comando
+
+crontab -e
+
+#Ejemplo de un crontab desde un archivo .sh
+
